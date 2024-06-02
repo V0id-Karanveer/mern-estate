e5eb1579528665407268004af830d0913487a3fb
V0id-Karanveer <karanveerkalsi10@gmail.com>
2024-06-02 20:10:59 +0530

    Create user update API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,38 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res) => {
   res.send("Hello Woreewfld!");
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "YOu can only update your own account!"));
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...others } = updatedUser._doc;
+
+    res.status(200).json(others);
+
+
+  } catch (error) {
+    next(error);
+  }
 };
