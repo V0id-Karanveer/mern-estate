9d5a0601be625834c600ff25a733f800cf1b8dc6
V0id-Karanveer <karanveerkalsi10@gmail.com>
2024-06-02 17:13:55 +0530

    Add Redux toolkit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,8 +7,8 @@ function Header() {
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to='/'>
                     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                        <span className='text-slate-500'>Kalsi</span>
-                        <span className='text-slate-700'>Estate</span>
+                        <span className='text-slate-500'>Petrolhead</span>
+                        <span className='text-slate-700'>Bazaar</span>
                     </h1>
                 </Link>
                 <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,85 +1,83 @@
-import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
-function SignUp() {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: ''
-  })
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-
   const navigate = useNavigate();
-
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id]: e.target.value
+      [e.target.id]: e.target.value,
     });
   };
-
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await axios.post('/api/auth/signup', formData);
-      const data = await res.data;
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
       if (data.success === false) {
-        setError(data.message); // Set the backend error message
         setLoading(false);
+        setError(data.message);
         return;
       }
-      else {
-        setLoading(false);
-        setError(null);
-        console.log(data.message)
-        navigate('/sign-in');
-      }
-    } catch(error){
       setLoading(false);
-      if(error.response){
-        console.error("Error:", error.response.data.message);
-        setError(error.response.data.message); // Handle axios/network error
-      }
-      else{
-        console.error("Error:", error.message);
-        setError(error.message); // Handle JavaScript error
-      }
-      
-    }    
-  }
-
-
-
-  console.log(formData);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="text" placeholder='username'
-          className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
 
-        <input type="email" placeholder='email'
-          className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-
-        <input type="password" placeholder='password'
-          className='border p-3 rounded-lg' id='password' onChange={handleChange} />
-
-        <button disabled={loading} className='bg-slate-700 text-white py-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign Up'}</button>
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
+        </button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to={"/sign-in"}>
+        <Link to={'/sign-in'}>
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
-      {error && <p className='text-red-500 mt-3'>{error}</p>}
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
-
-export default SignUp
