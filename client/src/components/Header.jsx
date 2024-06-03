a0d6c1a00e797467994c41225966a42b7d8880dd
V0id-Karanveer <karanveerkalsi10@gmail.com>
2024-06-03 10:55:43 +0530

    add listing api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,8 +9,8 @@ function Header() {
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to='/'>
                     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                        <span className='text-slate-500'>PetrolHead</span>
-                        <span className='text-cyan-500'>Bazaar</span>
+                        <span className='text-slate-500'>Ghar</span>
+                        <span className='text-cyan-500'>Search</span>
                     </h1>
                 </Link>
                 <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
